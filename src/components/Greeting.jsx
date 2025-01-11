e6c5094e1f574d164032006946c89f9ebddc10f7
Rijaul <rijaulsk001@gmail.com>
2025-01-11 11:18:45 +0530

    initial commit

---
+++ src/components/Greeting.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+const Greeting = () => {
+    const greet = "Hello";
+    const date = new Date();
+  return (
+    <div>
+      <h1>{greet}</h1>
+      <p>Date: {date.getDate()}</p>
+    </div>
+  );
+};
+
+export default Greeting;
